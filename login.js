c6e3ff4885b4017bcf3fd4cebfc53297acd34c6c
Matthieu V <18136478+gh-matv@users.noreply.github.com>
2023-01-25 17:46:10 +0900

    Feature/update login again (#7)

--- login.js
+++ login.js
@@ -1,7 +1,6 @@
 
 const montest = {x:"bonjour"};
 
-// Print the value of the variable montest 10 times
-for (let i = 0; i < 10; i++) {
-    console.log(montest.x);
+for (let i = 0; i < 20; i++) {
+    console.log(montest.x)
 }
