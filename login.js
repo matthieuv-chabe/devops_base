ef044cd8513064dd13c22ba83f19c63d693b3b2a
Matthieu VANCAYZEELE <112880161+matthieuv-chabe@users.noreply.github.com>
2023-01-25 17:29:47 +0900

    Feature/udpate login page (#6)
    * klmsjdqf
    
    * 10 fois
    
    * 10 foisqsdklf
    
    Co-authored-by: Matthieu Vancayzeele <matthieu.vancayzeele@gmail.com>

--- login.js
+++ login.js
@@ -1,5 +1,7 @@
-contenu 1
-changement 2
-changement 3
 
-aa
+const montest = {x:"bonjour"};
+
+// Print the value of the variable montest 10 times
+for (let i = 0; i < 10; i++) {
+    console.log(montest.x);
+}
